b36583ac4b53ec8b702f1ab8eb7e99c19b5c0829
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-06-23 04:51:42 +0530

    MongoDB Connecton URL added

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    'mongodb+srv://Babuli:<Ishani123>@cluster0.x8r8gcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
   )
     .then(client => {
       console.log('Connected!');
