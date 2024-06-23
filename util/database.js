369eb27813620763219170d9447bcb6893de3ef2
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-06-23 16:47:25 +0530

    MongoDB functionality added

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
+  console.log('mongoConnect :::: ');
   MongoClient.connect(
-    'mongodb+srv://Babuli:<Ishani123>@cluster0.x8r8gcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-  )
-    .then(client => {
+    'mongodb+srv://BabuliSahoo:Ishani@123@cluster0.x8r8gcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  ) .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
